032ad0868635358bc8f136a58a16809b9240e3c4
Zhijian Lai <53514478+GALVINLAI@users.noreply.github.com>
2024-01-09 21:52:26 +0900

    Update ProdLincomb.m

--- RIPM_ProductManifold/modified_supporting_functions/ProdLincomb.m
+++ RIPM_ProductManifold/modified_supporting_functions/ProdLincomb.m
@@ -1,25 +1,25 @@
 function prod = ProdLincomb(M, x, a1, s1, a2, s2)
-% 线性结构. Euc_eq是具体那种子空间没有影响.
+% Linear combination. The specific subspace of Euc_eq does not have any influence.
 
-% Linear combination function for product tangent space of
+% Linear combination function for the product tangent space of
 % M x Euc_eq x Euc_ineq x Euc_ineq 
-% appeared in RIPM.
+% appearing in RIPM.
 
 % function prod = ProdLincomb(M, x, a1, s1)
 % function prod = ProdLincomb(M, x, a1, s1, a2, s2)
 
-% only for 
+% Only for 
 % s.dx, s.dy,
 % and
 % s.dx, s.dy, s.dz, s.ds.
 
 if nargin == 4 % for ProdLincomb(M, x, a1, s1)
-    prod.dx = M.lincomb(x, a1, s1.dx);% This is valid since fun matrixlincomb() used in Manopt.
-    % 下段代码块足以应对结构体s的两种类型.
-    prod.dy = a1*s1.dy; % note that 1 * [] = [] in Matlab!
+    prod.dx = M.lincomb(x, a1, s1.dx); % This is valid since the function matrixlincomb() used in Manopt.
+    % The following code block is sufficient to handle two types of structs s.
+    prod.dy = a1*s1.dy; % Note that 1 * [] = [] in Matlab!
     if isfield(s1, 'dz')
-        %MODIFIED: For RIPM on ProductManifold
-        if isstruct(s1.dz)  
+        % MODIFIED: For RIPM on ProductManifold
+        if isstruct(s1.dz)
             prod.dz = multiplyStructWithScalar(s1.dz, a1);
             prod.ds = multiplyStructWithScalar(s1.ds, a1);
         else
@@ -31,11 +31,11 @@ if nargin == 4 % for ProdLincomb(M, x, a1, s1)
     end
 elseif nargin == 6 % for ProdLincomb(M, x, a1, s1, a2, s2)
     prod.dx = M.lincomb(x, a1, s1.dx, a2, s2.dx);
-    % 下段代码块足以应对结构体s的两种类型.
-    prod.dy = a1*s1.dy + a2*s2.dy; % note that  [] + [] = [] in Matlab!
+    % The following code block is sufficient to handle two types of structs s.
+    prod.dy = a1*s1.dy + a2*s2.dy; % Note that [] + [] = [] in Matlab!
     if isfield(s1, 'dz') && isfield(s2, 'ds')
-        %MODIFIED: For RIPM on ProductManifold
-        if isstruct(s1.dz)  
+        % MODIFIED: For RIPM on ProductManifold
+        if isstruct(s1.dz)
             prod.dz = addStructs(multiplyStructWithScalar(s1.dz, a1), multiplyStructWithScalar(s2.dz, a2));
             prod.ds = addStructs(multiplyStructWithScalar(s1.ds, a1), multiplyStructWithScalar(s2.ds, a2));
         else
